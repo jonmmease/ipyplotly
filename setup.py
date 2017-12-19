d8f75b4026d9fb73a1a9968a629781b402e6cfda
Jon M. Mease <jon.mease@jhuapl.edu>
2017-12-19 13:54:20 -0500

    Missing comma

--- setup.py
+++ setup.py
@@ -159,7 +159,7 @@ setup_args = {
         'numpy>=1.13',
         'pandas>=0.20',
         'pillow>=4.2',
-        'cairosvg>=2.0.0rc6'
+        'cairosvg>=2.0.0rc6',
         'plotly>=2.1'
     ],
     'packages': find_packages(exclude=('codegen',)),
